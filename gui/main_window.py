812dee102e0f404d4106ef1ed69706d5b99b0ef7
cactuarix <kalinkinaleksej9@gmail.com>
2024-12-14 14:18:11 +0300

    Добавил окно для ввода информации о пользователе при добавлении. Добавил venv в .gitignore

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,7 +1,33 @@
-from PyQt6.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget, QMessageBox
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget, QMessageBox, QApplication, QDialog, QLineEdit, QLabel, QDialogButtonBox
 from app.handlers import add_user, clear_tables
 from app.database import get_db
 
+
+class InputDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Введите параметры")
+        
+        # Создаем элементы для ввода
+        self.label = QLabel("Введите параметры:")
+        self.input_line = QLineEdit(self)
+
+        # Кнопки OK и Cancel
+        button_box = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel)
+        button_box.accepted.connect(self.accept)
+        button_box.rejected.connect(self.reject)
+
+        # Устанавливаем layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.input_line)
+        layout.addWidget(button_box)
+        self.setLayout(layout)
+
+    def get_input(self):
+        return self.input_line.text()
+    
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -24,9 +50,15 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(container)
 
     def add_user(self):
-        with next(get_db()) as db:
-            add_user(db, "Иван Иванов", "89001234567", "клиент")
-            QMessageBox.information(self, "Успех", "Пользователь добавлен!")
+        dialog = InputDialog()
+        if dialog.exec() == QDialog.DialogCode.Accepted:
+            user_input = dialog.get_input()
+            with next(get_db()) as db:
+                try:
+                    add_user(db, *user_input.split(' '))
+                    QMessageBox.information(self, "Успех", "Пользователь добавлен!")
+                except TypeError:
+                    QMessageBox.information(self, "Неудача", "Неправильные аргументы")
 
     def clear_tables(self):
         with next(get_db()) as db:
